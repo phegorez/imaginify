6a380e688807516e1690a3de0b4ec14682ca932c
phegorez <metha.nompd@gmail.com>
2025-03-12 15:40:50 +0700

    save

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,4 +1,4 @@
-import { Document, model, models, Schema } from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 // clerkId, email, username, photo, firstName, LastName, planId, creditBalance
 
